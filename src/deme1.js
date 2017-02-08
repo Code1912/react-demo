b4f6ea917f30306b247997c20f45e79aea48f32a
Code1912 <code1912@163.com>
2017-02-08 23:48:23 +0800

    add demo1

---
+++ src/deme1.js
@@ -0,0 +1,40 @@
+/**
+ * Created by Code1912 on 2017/2/8.
+ */
+import  React from "react"
+import  ReactDOM from "react-dom"
+
+class Demo1 extends  React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {text: props.text||'',title:"title"};
+    }
+    onChangeTextClick(){
+        if(!this){
+            return;
+        }
+
+        this.setState( {text:"text  changed"})
+    }
+
+    onTextChange(e){
+        if(e.target.value!=this.state.text){
+            this.setState( {text:e.target.value})
+        }
+    }
+    render(){
+        //<!-- <input value="" defaultValue='a'/>  同时写value 和default value 要报错   单独写value='ada' 也要报错 ，默认赋值得写defaultValue='abc'-->
+        return  <div>
+                         <hr/>
+                         <h1>Demo1  {this.state.title}</h1>
+                         Default Value:<input defaultValue="ss"  />  <br/>
+                         Text Label:<span>{this.state.text}</span> <br/>
+                         Text Input:<input type="text" value={this.state.text}
+                                           onChange={this.onTextChange.bind(this)}/>
+                                    <button onClick={this.onChangeTextClick.bind(this)}>Changed Text</button>
+
+                </div>
+    }
+}
+
+export default Demo1
--- src/index.js
+++ src/index.js
@@ -6,10 +6,13 @@ import  ReactDOM from "react-dom"
 import  TestComponent from "./testComponent"
 //ReactDOM.render(<h1>Hello, world!</h1>, document.getElementById("app"));
 //ReactDOM.render(<TestComponent/>, document.getElementById("app"));
-
+import  Demo1 from "./deme1"
+// chrome react developer tools  download page:https://chrome.google.com/webstore/detail/react-developer-tools/fmkadmapgofadopljbjfkapdkoienihi
+// react developer tools introduction: https://facebook.github.io/react/blog/2015/09/02/new-react-developer-tools.html
 ReactDOM.render(
     <div>
         <h1>Hello, world!</h1>
         <TestComponent/>
+        <Demo1 text="abc"/>
     </div>,
     document.getElementById("app"));
